542ec01b889baf66a99ef6d216d396a9302af643
piwniczak2137 <70859223+thesun901@users.noreply.github.com>
2025-01-31 00:28:04 +0100

    feat: add conventional messaging

---
+++ server/consts.py
@@ -0,0 +1,10 @@
+BROKER_IP = "10.0.2.15"
+BROKER_PORT = 1883
+SENDER_NAME = "parking_server"
+
+TOPICS = {
+    "ui" : "/ui",
+    "entry" : "/entries",
+    "departure" : "/departures",
+    "db" : "/database"
+}
