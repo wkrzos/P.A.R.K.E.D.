6f2f72305d26f6be41e9c82f906aa5fae1fb07ac
Wojciech Krzos <wojciechkrzos67@gmail.com>
2025-02-01 21:05:51 +0100

    feat: add database seeding functionality with sample data

---
+++ db/consts.py
@@ -0,0 +1,11 @@
+BROKER_IP = "localhost"
+BROKER_PORT = 1883
+SENDER_NAME = "dbcontroller"
+
+DB_CONFIG = {
+    "host": "localhost",
+    "port": "5432",
+    "database": "parked",
+    "user": "postgres",
+    "password": "postgres"
+}
